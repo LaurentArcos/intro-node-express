caca8868f2e485cffe314154deb90f5b1a275360
LaurentArcos <laurent.arcos@gmail.com>
2024-02-21 09:59:19 +0100

    ajout de dummy datas

--- main/index.ts
+++ main/index.ts
@@ -5,13 +5,32 @@ const port:number = 3000
 const app = express()
 
 
+const users = [
+    {
+        name : 'John',
+        lastName : 'Doe',
+        age : 32
+    },
+    {
+        name : 'Jane',
+        lastName : 'Doe',
+        age : 34
+    }
+]
+
+
 app.get('/', (req : Request, res : Response) => {
-  res.send('Welcome to our first API using node')
+    res.send('Welcome to our first API using node')
 })
 
 app.get('/users', (req : Request, res : Response) => {
-  res.send('Users route')
+    res.json(users)
 })
+
+app.get('/api/:name', (req : Request, res : Response ) => {
+    console.log(req.params)
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
