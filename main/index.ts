3cc586070c2e2975a78443fed28f632c579897e6
LaurentArcos <laurent.arcos@gmail.com>
2024-02-21 11:14:58 +0100

    ajout de roytes post pu et delete

--- main/index.ts
+++ main/index.ts
@@ -1,6 +1,14 @@
 import {Request, Response} from 'express-serve-static-core'
 import express from 'express'
 import usersRoute from './routes/usersRoute'
+import moviesRoute from './routes/moviesRoute'
+
+
+
+express.urlencoded({
+  extended : false
+})
+
 const port:number = 3000
 const app = express()
 
@@ -8,12 +16,12 @@ const app = express()
 
 
 app.get('/', (req : Request, res : Response) => {
-    res.send('Welcome to our first API using node')
+  res.send('Welcome to our first API using node')
 })
 
 
-app.use(usersRoute)
+app.use('/api', usersRoute, moviesRoute)
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`)
 })
--- main/routes/moviesRoute.ts
+++ main/routes/moviesRoute.ts
@@ -6,22 +6,62 @@ const moviesRoute = express.Router()
 const movies = [
     {
         id : 1,
-        name : 'Poor things',
-        genre : 'Sci-fi'
+        name : 'The Lord Of The Rings',
+        genre : 'Fantasy'
     },
     {
         id : 2,
-        name : 'Mais qui a tué pamela rose',
+        name : 'Zodiac',
         genre : 'Thriller'
     },
     {
         id : 3,
-        name : 'Gladiator',
+        name : 'Kingdom Of Heaven',
         genre : 'Historic'
     }
 ]
 
 
 moviesRoute.get('/movies', (req : Request, res : Response) => {
-    res.json()
+  res.json(movies)
 })
+
+
+moviesRoute.get('/movies/:id', (req : Request, res : Response) => {
+  let movieId = parseInt(req.params.id)
+  // connection.query(`SELECT * FROM movies where id = ? `, movieId)
+  let movie = movies.find(movie => movie.id === movieId)
+  res.json(movie)
+})
+
+
+moviesRoute.post('/movies', (req : Request, res : Response) => {
+  // connection.query(`INSERT INTO movies VALUES(?,?)`, req.body.name, req.body.genre)
+  let movieId = movies.length + 1
+  let movie = {
+      id : movieId,
+      name : req.body.name,
+      genre : req.body.genre,
+  }
+  movies.push(movie)
+  res.json(movies)
+})
+
+
+moviesRoute.put('/movies/:id', (req : Request, res : Response) => {
+  let movieId = parseInt(req.params.id)
+  let movie = movies.find(movie => movie.id === movieId)
+  movie!.name = req.body.name
+  res.json(movie)
+})
+
+moviesRoute.delete('/movies/:id', (req : Request, res : Response ) => {
+  const movieId = parseInt(req.params.id)
+  const movie = movies.find(movie => movie.id === movieId)
+  const index = movies.indexOf(movie!)
+  movies.splice(index, 1)
+  res.send('Movie deleted')
+})
+
+
+export default moviesRoute
