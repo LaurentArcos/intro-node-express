48243d8416bee48934eb949dac57433e7f6004fa
LaurentArcos <laurent.arcos@gmail.com>
2024-02-21 12:15:00 +0100

    ajout Docker

--- main/index.ts
+++ main/index.ts
@@ -2,26 +2,23 @@ import {Request, Response} from 'express-serve-static-core'
 import express from 'express'
 import usersRoute from './routes/usersRoute'
 import moviesRoute from './routes/moviesRoute'
-
-
-
-express.urlencoded({
-  extended : false
-})
-
 const port:number = 3000
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended : false
+}))
 
 
 
 app.get('/', (req : Request, res : Response) => {
-  res.send('Welcome to our first API using node')
+    res.send('Welcome to our first API using node')
 })
 
 
 app.use('/api', usersRoute, moviesRoute)
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+    console.log(`Server is running on port ${port}`)
 })
