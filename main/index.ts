668f1bea79b4a9a9ba1258c0d658b05888230633
LaurentArcos <laurent.arcos@gmail.com>
2024-02-22 11:11:26 +0100

    ajout mongoose

--- main/index.ts
+++ main/index.ts
@@ -2,13 +2,21 @@ import {Request, Response} from 'express-serve-static-core'
 import express from 'express'
 import usersRoute from './routes/usersRoute'
 import moviesRoute from './routes/moviesRoute'
+import Mongoose from 'mongoose'
 const port:number = 3000
 const app = express()
+require('dotenv').config()
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended : false
 }))
+
+Mongoose.connect(process.env.MONGO_URI as string)
+
+const db = Mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 
 
--- main/routes/usersRoute.ts
+++ main/routes/usersRoute.ts
@@ -1,19 +1,64 @@
 import {Request, Response} from 'express-serve-static-core'
 import express from 'express'
+import User from '../models/User'
 const usersRoute = express.Router()
 
 
 
-usersRoute.get('/users', (req : Request, res : Response) => {
-    res.send('Users route')
+usersRoute.get('/users', async  (req : Request, res : Response) => {
+    try{
+        // On vient recuperer le model user et on applique la methode .find() qui va nous permettre de recuperer les documents dans la collection User
+        const user = await User.find()
+        res.json(user)
+    }
+    catch(err){
+        console.error(err)
+    }
+
 })
 
-usersRoute.get('/users/:name/:id', (req : Request, res : Response) => {
-    console.log(req.params)
-    res.send(`Welcome ${req.params.name}`)
+
+usersRoute.get('/users/:id', (req : Request, res : Response) => {
+    // On vient recuperer le document grace a son id en utilisant la methode findOne et en lui passant req.params.id qui est le parametre dans notre URL (:id)
+    User.findOne({_id : req.params.id})
+    .then(user => res.json(user))
+    .catch(err => console.error(err))
+})
+
+
+
+usersRoute.post('/users', (req : Request, res : Response) => {
+    // On utilise la methode create afin d'ajouter un utilisateur, en passe comme parametre req.body qui est le corp de notre requete 
+    User.create(req.body)
+    .then(newUser => res.json(newUser))
+    .catch(err => console.error(err))
+})
+
+usersRoute.put('/users/:id', async (req : Request, res : Response) => {
+    // On veut update un utilisateur, on va d'abord recuperer un utilisateur par son id avec la methode findOne et ensuite utiliser updateOne afin de le modifier
+    try{
+        const findUser:any = await User.findOne({_id : req.params.id})
+        const updateUser = await findUser.updateOne({$set : req.body})
+        res.json(updateUser)
+    }
+    catch(err){
+        console.error(err)
+    }
+
+})
+
+
+usersRoute.delete('/users/:id', async (req: Request, res : Response) => {
+    // On vient delete un utilisateur par son id, on recupere l'id dans notre param d'url encore une fois req.params.id
+    try{
+        const deleteUser = await User.deleteOne({_id : req.params.id})
+        res.json('User has been deleted')
+    }
+    catch(err){
+        console.error(err)
+    }
 })
 
 
 
 export default usersRoute
-
